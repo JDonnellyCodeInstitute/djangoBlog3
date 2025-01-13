b647fabfdc2d6f800290b38be32d159a5dda9b3e
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2025-01-13 12:32:48 +0000

    Can now successfully post a comment to the database as an authenticated user

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -31,6 +33,22 @@ def post_detail(request, slug):
     # Number of approved comments
     comment_count = post.comments.filter(approved=True).count()
 
+    # Post request for the comment
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    # Box for users to input comments
+    comment_form = CommentForm()
+
     return render(
         request,
         "blog/post_detail.html",
@@ -38,5 +56,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )

ff8843eedb82ad1bd36bf53c7dcb86e9d4329ce0
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2025-01-13 12:14:02 +0000

    Comments and comment count now displayed on posts

--- blog/views.py
+++ blog/views.py
@@ -26,9 +26,17 @@ def post_detail(request, slug):
     queryset = Post.objects.filter(status=1) 
     # This stops incorrect inputs - if the slug is input incorrectly, or theres a 404 this activates 
     post = get_object_or_404(queryset, slug=slug)
+    # All comments ordered in terms of recency
+    comments = post.comments.all().order_by("-created_on")
+    # Number of approved comments
+    comment_count = post.comments.filter(approved=True).count()
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
     )
